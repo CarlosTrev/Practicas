0732686ae2ef5c8d538f350b4b5d75a2b68a017c
CarlosTrev <carlostrevinomtz1@gmail.com>
2025-09-08 13:42:42 -0600

    Se agrega el Textfield

--- app/src/main/java/com/example/practicas/MainActivity.kt
+++ app/src/main/java/com/example/practicas/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.practicas
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
@@ -9,20 +11,29 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.Button
+import androidx.compose.material3.TextField
+import androidx.compose.material3.Text
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -48,17 +59,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun PantallaPrincipal(name: String, modifier: Modifier = Modifier) {
+    var nombre by remember { mutableStateOf("Carlos Adrian Treviño")}
     Column (modifier = modifier
         .fillMaxSize()
         .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center){
-        Text(
-            text = "Carlos Adrian Treviño",
-            color = Color.Black,
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center
+        TextField(
+            value = nombre,
+            onValueChange = { nombre = it },
+            modifier = Modifier
+                .fillMaxWidth(0.8f), // 80% del ancho de la pantalla
+            textStyle = androidx.compose.ui.text.TextStyle(
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
         )
         Spacer(modifier = Modifier.height(20.dp))
 
