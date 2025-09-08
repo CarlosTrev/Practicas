037311b99c9afcbbd3d3dbbe4295b134f963d9dd
CarlosTrev <carlostrevinomtz1@gmail.com>
2025-09-08 13:30:19 -0600

    Se agregan nombre y boton centrados, con un poco (muy poco) estilo

--- app/src/main/java/com/example/practicas/MainActivity.kt
+++ app/src/main/java/com/example/practicas/MainActivity.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.material3.Button
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.practicas.ui.theme.PracticasTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +36,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PracticasTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    PantallaPrincipal(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -31,17 +47,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun PantallaPrincipal(name: String, modifier: Modifier = Modifier) {
+    Column (modifier = modifier
+        .fillMaxSize()
+        .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center){
+        Text(
+            text = "Carlos Adrian Treviño",
+            color = Color.Black,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.height(20.dp))
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PracticasTheme {
-        Greeting("Android")
+        Button(
+            onClick = { /* TODO */ },
+            shape = CutCornerShape(40.dp),
+            border = BorderStroke(3.dp, Color.Black),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Red,
+                contentColor = Color.White
+            ),
+            modifier = Modifier
+                .width(150.dp)
+                .height(50.dp)
+        ) {
+            Text(text = "Enviar", fontSize = 18.sp)
+        }
     }
 }
